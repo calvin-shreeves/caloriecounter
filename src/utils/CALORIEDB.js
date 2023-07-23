e8999469d92a4e4a754ff3e3490eadaf8c2acd80
Calvin Shreeves <calvin.shreeves@gmail.com>
2023-07-23 15:43:52 -0400

    init db schemas

---
+++ src/utils/CALORIEDB.js
@@ -0,0 +1,10 @@
+const CALORIES = [
+  {
+    name: "Chicken",
+    "serving size": "4 oz",
+    fat: "3 g",
+    carbs: "0",
+    protein: "24 g",
+    brand: "tysons",
+  },
+];
---
+++ src/utils/USERDB.js
@@ -0,0 +1,26 @@
+const USERS = [
+  {
+    name: "CJ",
+    photo: "photo",
+    height: "5 ft, 8in",
+    sex: "male",
+    dob: "1/1/2020",
+    location: "usa",
+    email: "email@example.com",
+    phone: "123-123-1234",
+    goal: Goals[1],
+    "calorie needs": 2500,
+    "daily calorie count": [2500, 1100],
+    "daily meals": ["<meals>"],
+  },
+];
+
+const Goals = ["bulking", "cutting", "maintaining"];
+
+const GoalBreakDown = {
+  "starting weight": "150lb",
+  "current weight": "150lb",
+  "goal weight": "170lb",
+  "weekly goal": "1 lb / week",
+  "activity level": "moderately active",
+};
